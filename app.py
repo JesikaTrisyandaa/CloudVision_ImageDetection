eed1f09f154b91ff367632b402863b3e1b0234c6
JesikaTrisyandaa <jesikatrisyandaaa@gmail.com>
2026-06-24 13:01:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 import io
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=".", static_url_path="")
 CORS(app)
 
-model = tf.keras.models.load_model(r"model_akurasi72.h5", compile=False)
+model = tf.keras.models.load_model("model_akurasi72.h5", compile=False)
 
 CLOUD_CLASSES = [
     "Altocumulus",
@@ -24,11 +25,14 @@ CLOUD_CLASSES = [
     "Stratus"
 ]
 
-
+@app.route("/")
+def home():
+    return send_from_directory(".", "index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
     file = request.files["image"]
+
     img = Image.open(io.BytesIO(file.read())).convert("RGB")
     img = img.resize((224, 224))
 
@@ -41,11 +45,11 @@ def predict():
         {"class": CLOUD_CLASSES[i], "prob": float(probs[i])}
         for i in range(len(CLOUD_CLASSES))
     ]
+
     predictions = sorted(predictions, key=lambda x: x["prob"], reverse=True)
 
     return jsonify({"predictions": predictions})
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
